4cc7091c434c2fa5c094281ba5e2f71421ddb1bf
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2020-02-12 01:10:11 -0300

    version 2

--- src/Actividades.js
+++ src/Actividades.js
@@ -5,17 +5,29 @@ export default class Actividades extends Component {
   actividades = (cantidad) => {
     let actividades = []
     for (var i = 1; i <= cantidad; i++) {
-      const avance = (Math.random()*100).toFixed(0)+'%';
-      const esperado = (Math.random()*100).toFixed(0)+'%';
-      const avanceReal = (Math.random()*100).toFixed(0)+'%';
+      const avance = (Math.random()*100).toFixed(0);
+      const esperado = (Math.random()*100).toFixed(0);
+      let estilo = '';
+      let diferencia = avance - esperado;
+      if (diferencia < -10) {
+        estilo = 'bg-danger';
+      } else if (diferencia >= -10 && diferencia < -5) {
+        estilo = 'bg-warning';
+      } else if (diferencia >= -5 && diferencia < 0) {
+        estilo = 'bg-info'
+      } else if (diferencia >= 0 && diferencia < 5) {
+        estilo = ''
+      } else if (diferencia >= 5) {
+        estilo = 'bg-success'
+      }
       actividades.push(
-        <div className="row border-bottom mx-0" style={{backgroundColor: '#EEe333'}}>
+        <div className="row border-bottom mx-0" style={{backgroundColor: '#ee8572'}}>
           <div className="col-lg-4 border-right border-dark text-left h5 m-0">{'Actividad ' +i}</div>
-          <div className="col-lg-1 border-right border-dark text-center h5 m-0">{esperado}</div>
-          <div className="col-lg-1 border-right border-dark text-center h5 m-0">{avance}</div>
+          <div className="col-lg-1 border-right border-dark text-center h5 m-0">{esperado+' %'}</div>
+          <div className="col-lg-1 border-right border-dark text-center h5 m-0">{avance+' %'}</div>
           <div className="col-lg-6 text-center h5 m-0">
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" style={{width: avanceReal}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            <div className="progress m-2">
+              <div className={"progress-bar "+ estilo} role="progressbar" style={{width: avance+'%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{avance+' %'}</div>
             </div>
           </div>
         </div>
--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ class App extends Component {
 
   render() {
     return (
-      <div className="border m-3">
+      <div className="border my-3" style={{backgroundColor: '#63b7af'}}>
         <div className="row my-3">
           <div className="col-lg-12 text-center h1">Proyecto | Aplicación web de Gestión de Proyectos</div>
         </div>
-        <div className="row border-top mx-0" style={{backgroundColor: 'tomato'}}>
+        <div className="row border-top mx-0" style={{backgroundColor: '#347474'}}>
           <div className="col-lg-4 border-right border-dark text-center h5 m-0">Actividad</div>
           <div className="col-lg-1 border-right border-dark text-center h5 m-0">Esperado</div>
           <div className="col-lg-1 border-right border-dark text-center h5 m-0">Avance</div>
           <div className="col-lg-6 text-center h5 m-0">Progreso</div>
         </div>
-        {<Actividades cantidad={12} />}        
+        {<Actividades cantidad={12} />}
       </div>
     );
   }
